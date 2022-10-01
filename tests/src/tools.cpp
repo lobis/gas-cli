a75f339820aa563ee26b75f74b46cadf573e1afb
Luis Obis <luis.antonio.obis@gmail.com>
2022-10-01 16:45:16 +0200

    added tests

---
+++ tests/src/tools.cpp
@@ -0,0 +1,13 @@
+
+#include <Gas.h>
+
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+using namespace std;
+
+TEST(Tools, SortingElectricFieldForSimulation) {
+}
