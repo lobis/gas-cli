13b09dc574e78cd2246b66121405c0f53c6fc245
Luis Obis <luis.antonio.obis@gmail.com>
2022-12-01 10:40:04 +0100

    add mostly empty files for condor example

---
+++ examples/condor/condor.py
@@ -0,0 +1,7 @@
+# https://hub-binder.mybinder.ovh/user/htcondor-htcond-dings-tutorials-kj4m1nl1/lab/tree/DAG-Creation-And-Submission.ipynb
+
+import htcondor
+import classad
+
+if __name__ == "__main__":
+    pass
